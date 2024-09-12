6f5953ac48c213a7714a992f04553ba145696331
jacobsteffenBYU <jacob.steffen@byu.edu>
2024-09-12 16:47:43 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # a2-submitting-to-github
 IS 303 assignment. Practice for running tests on your code and uploading to GitHub.
+hello **this is bold text** 
---
+++ a2_submitting_to_github copy.py
@@ -0,0 +1,5 @@
+# Name:
+# Description:  Enter your code below. You can test your code using the testing tab on the left.
+#               When finished, upload your code by going to the source control tab, committing your code, and then syncing it.
+#               Replace this text with your own description of what your code does.
+
